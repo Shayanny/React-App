1360dce00e15cc92529320fb487256e04eafb4be
Shayanny <shayanny4@outlook.com>
2024-12-08 00:22:38 +0000

    Centered Nav bar

--- my-project/src/components/Navbar.js
+++ my-project/src/components/Navbar.js
@@ -8,10 +8,10 @@ import Navbar from 'react-bootstrap/Navbar';
 const NavigationBar = () => {
 
     return(
-        <Navbar bg="dark" data-bs-theme="dark">
+        <Navbar bg="dark" data-bs-theme="dark" className="justify-content-center">
           <Container>
             <Navbar.Brand>Navbar</Navbar.Brand>
-            <Nav className="me-auto">
+            <Nav className="mx-auto">
               <Nav.Link href="/Home">Home</Nav.Link>
               <Nav.Link href="/Create">Create</Nav.Link>
               <Nav.Link href="/Read">Read</Nav.Link>
