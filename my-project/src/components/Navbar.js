1aec7dcb5e116f47568fabaaf2f9f73cd011df45
Shayanny <shayanny4@outlook.com>
2024-12-17 23:35:19 +0000

    Added an icon to  navbar

--- my-project/src/components/Navbar.js
+++ my-project/src/components/Navbar.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import { Nav } from "react-bootstrap";
 import Navbar from 'react-bootstrap/Navbar';
+import { GiForkKnifeSpoon } from "react-icons/gi"; // Import icon from react icons
+
 
 
 
@@ -10,7 +12,9 @@ const NavigationBar = () => {
     return(
         <Navbar bg="dark" data-bs-theme="dark" className="justify-content-center">
           <Container>
-            <Navbar.Brand>Navbar</Navbar.Brand>
+            <Navbar.Brand>
+            <GiForkKnifeSpoon style={{ marginRight: "8px", fontSize: "1.5rem" }} />
+              Munchies</Navbar.Brand>
             <Nav className="mx-auto">
               <Nav.Link href="/Home">Home</Nav.Link>
               <Nav.Link href="/Create">Create</Nav.Link>
